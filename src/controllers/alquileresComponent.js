9b36656c13570b8cf050c6e2a66730d035f31374
nahhueee <nahu852na@gmail.com>
2021-11-25 22:25:50 -0300

    25_11

--- src/controllers/alquileresComponent.js
+++ src/controllers/alquileresComponent.js
@@ -3,10 +3,33 @@ const db = require ('../../db');
 
 class AlquileresControl{
     ConsultarDestacados (req, res){
-        db.default.query(`SELECT * FROM Alquileres WHERE destacado = 1 ORDER BY RAND()`, [req.params.id],(error, stock) => {
-                if (error) throw error;
-                res.json(stock);
+        db.default.query(`SELECT a.*, FORMAT(IFNULL(SUM(c.raiting)/COUNT(c.raiting),0),2) Raiting, z.nombre zona from alquileres a 
+                        LEFT JOIN comentarios c on a.id = c.idAlquiler
+                        INNER JOIN zonas z on z.id = a.idZona
+                        WHERE destacado = 1
+                        GROUP BY a.id
+                        ORDER BY RAND()`, [req.params.id],(error, stock) => {
+                        if (error) throw error;
+                        res.json(stock);
             });
+    };
+
+    ObtenerAlquileres (req, res){
+        const data = req.body;
+        console.log(data)
+
+        if(data.Localidad!=undefined){
+            db.default.query(`SELECT a.*, FORMAT(IFNULL(SUM(c.raiting)/COUNT(c.raiting),0),2) Raiting, z.nombre zona from alquileres a 
+            LEFT JOIN comentarios c on a.id = c.idAlquiler
+            INNER JOIN zonas z on z.id = a.idZona
+            WHERE a.idZona = ?
+            GROUP BY a.id
+            ORDER BY a.id DESC`, [data.Localidad],(error, stock) => {
+            if (error) throw error;
+            res.json(stock);
+            });
+        }
+
     };
 }
 
--- src/routes/alquileres.js
+++ src/routes/alquileres.js
@@ -4,5 +4,6 @@ const router  = Router();
 
 //router.get('/', cajasControl.Consultar);
 router.get('/', AlquilerControl.default.ConsultarDestacados);
+router.put('/', AlquilerControl.default.ObtenerAlquileres);
 
  exports.default = router;
